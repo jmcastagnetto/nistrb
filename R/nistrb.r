a379ab381fa3a1c76d3e8b9f89ef7cc911bea697
Jesus M. Castagnetto <jesus@castagnetto.com>
2015-12-28 08:34:27 -0500

    Fixed warnings and notes on descriptions

--- R/nistrb.r
+++ R/nistrb.r
@@ -6,13 +6,13 @@
 #' http://www.nist.gov/itl/csd/ct/nist_beacon.cfm
 #'
 #' The service returns an XML document with several pieces of information:
-#' \itemize {
-#'    \item {The (seed) value represented as a 64 byte (512-bit) hex string value, and its timestamp}
-#'    \item {The SHA-512 hash value for the previous record as a 64 byte hex string}
-#'    \item {A digital signature (RSA) computed over (in order): version, frequency, timeStamp, seedValue, previousHashValue, errorCode. Note: Except for version, the hash is on the byte representations and not the string representations of the data values}
-#'    \item {The SHA-512 hash of the signature as a 64 byte hex string}
-#'    \item {The service version and its update frequency}
-#'    \item {The status of the response}
+#' \itemize{
+#'    \item{The (seed) value represented as a 64 byte (512-bit) hex string value, and its timestamp}
+#'    \item{The SHA-512 hash value for the previous record as a 64 byte hex string}
+#'    \item{A digital signature (RSA) computed over (in order): version, frequency, timeStamp, seedValue, previousHashValue, errorCode. Note: Except for version, the hash is on the byte representations and not the string representations of the data values}
+#'    \item{The SHA-512 hash of the signature as a 64 byte hex string}
+#'    \item{The service version and its update frequency}
+#'    \item{The status of the response}
 #' }
 #'
 #' @docType package
